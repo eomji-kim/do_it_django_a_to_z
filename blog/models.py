1e7ffdd078471181a4fdb7921415e38cfed2fdb9
eomji-kim <kejn333@naver.com>
2022-12-01 14:44:19 +0900

    삭제된 작성자는 None으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 class Post(models.Model):
@@ -12,10 +13,13 @@ class Post(models.Model):
     # auto_now_add : 현 시간에 맞추고, 없으면 추가하기
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     # author : 외래키 추후 작성 예정
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
 
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'

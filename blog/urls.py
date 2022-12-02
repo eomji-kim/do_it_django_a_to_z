c282c458b11015d9e31ff0a270d62a3fe130e50b
eomji-kim <kejn333@naver.com>
2022-12-02 11:56:50 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from  . import  views
 
 urlpatterns = [
+    # slug 인자 : url localhost:8000/blog/category/programming/ 이라고 입력하면
+    # programming/ 만 떼어 views.py의 category_page()함수로 보냄.
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
     # path('', views.index),

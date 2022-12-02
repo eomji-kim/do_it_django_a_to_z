b7c30d46fad5a5a7ee21030ac6bc623d1816a303
eomji-kim <kejn333@naver.com>
2022-12-02 16:03:01 +0900

    태그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from  . import  views
+from . import views
 
 urlpatterns = [
-    # slug 인자 : url localhost:8000/blog/category/programming/ 이라고 입력하면
-    # programming/ 만 떼어 views.py의 category_page()함수로 보냄.
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
-    # path('', views.index),
-    # path('<int:pk>/' , views.single_post_page),
 ]
